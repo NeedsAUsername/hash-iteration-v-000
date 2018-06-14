745272a2a232034f8d9e4612574c82a00aa605d3
Kevin Chi <kevinchi118@gmail.com>
2018-06-14 18:39:19 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
     winner = ""
-    passengers.each do |passenger|
-        if 
+    passengers.each do |suite, name|
+        if suite == "suite_a" $$ name == 
     end
 end
